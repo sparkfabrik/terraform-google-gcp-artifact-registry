35e5093a28036aabc7af7b37e58a0dc32deee2cf
Stefano Sibilia <stefano.sibilia@sparkfabrik.com>
2024-10-15 12:01:46 +0200

    fix: remote repositories condition

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ locals {
       username_password_credentials_password_secret_version = repository.username_password_credentials_password_secret_version
       username_password_credentials_username                = repository.username_password_credentials_username
     }
-    if repository.remote_repository_config_docker.username_password_credentials_password_secret_version != ""
+    if repository.remote_repository_config_docker != ""
   }
 }
 
